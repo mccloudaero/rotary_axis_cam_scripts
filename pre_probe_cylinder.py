23f929c309cf5d6179f359225b8193f70b6e756d
Peter McCloud <peter@mccloudaero.com>
2022-05-28 14:08:35 -0700

    pre_probe_cylinder.py - renamed inputs dict

--- pre_probe_cylinder.py
+++ pre_probe_cylinder.py
@@ -6,36 +6,53 @@ import math
 #   1) The center of the cylinder is along the Y=0, Z=0 axis
 #   2) X = 0 is the start of the cylinder
 
-inputs = {
-    'outer_diameter' : 6.0,
-    'num_x_points' : 1,
-    'num_a_points' : 12,
-    'start_x' : 0,
+pre_probe_inputs = {
+    'outer_diameter' : 12.75,
+    'num_x_points' : 4,
+    'num_a_points' : 24,
+    'start_x' : -3.0,
     'end_x' : 0,
     'safe_clearance': 0.1,
     'min_probe_depth': -0.1,
     'probe_feedrate': 1.0,
+    'output_file': None,
 }
 
-total_points = inputs['num_a_points']*inputs['num_x_points']
-outer_radius = inputs['outer_diameter']/2.0
-safe_z_height = outer_radius + inputs['safe_clearance']
-probe_min_z = outer_radius + inputs['min_probe_depth']
+print('Creating pre-probe G-code file')
+total_points = pre_probe_inputs['num_a_points']*pre_probe_inputs['num_x_points']
+outer_radius = pre_probe_inputs['outer_diameter']/2.0
+safe_z_height = outer_radius + pre_probe_inputs['safe_clearance']
+probe_min_z = outer_radius + pre_probe_inputs['min_probe_depth']
 
-if inputs['num_x_points'] < 1:
+if pre_probe_inputs['num_x_points'] < 1:
     print('\nInvalid number of X points\nExiting!')
     sys.exit(1)
-if inputs['num_x_points'] == 1:
-    delta_x = None
+if pre_probe_inputs['num_x_points'] == 1:
+    delta_x = 0
 else:
-    delta_x = (inputs['end_x'] - inputs['start_x'])/(inputs['num_x_points']-1)
-delta_a = 360.0/inputs['num_a_points']
+    delta_x = (pre_probe_inputs['end_x'] - pre_probe_inputs['start_x'])/(pre_probe_inputs['num_x_points']-1)
+delta_a = 360.0/pre_probe_inputs['num_a_points']
 
-# Open File
-output_file = open('pre_probe.nc','w')
+print('Number of X Points: {:3d}'.format(pre_probe_inputs['num_x_points']))
+print('Number of A Points: {:3d}'.format(pre_probe_inputs['num_a_points']))
+print('Total Number of Points {:3d}'.format(total_points))
+
+
+# Open Output File
+if pre_probe_inputs['output_file'] is None:
+    # Autocreate filename
+    output_filename = 'pre_probe_'
+    output_filename += str(pre_probe_inputs['outer_diameter']) + '_od'
+    output_filename += '.nc'
+else:
+    output_filename = pre_probe_inputs['output_file']
+print('\nWriting Gcode to:', output_filename)
+output_file = open(output_filename,'w')
+
 
 # Write Header
 output_file.write('(G-code automatically written using pre_probe_cylinder.py)\n')
+output_file.write('(Outer Diameter: {:4.2f})\n'.format(pre_probe_inputs['outer_diameter']))
 output_file.write('(Total Number of points to Probe: {:4d})\n'.format(total_points))
 output_file.write('G90   (set absolute distance mode)\n')
 output_file.write('G90.1 (set absolute distance mode for arc centers)\n')
@@ -51,14 +68,14 @@ output_file.write('\n')
 output_file.write('G0 Z {:5.4f} (Safe Z height)\n'.format(safe_z_height))
 output_file.write('G0 Y 0.0000\n')
 
-for i in range(inputs['num_x_points']):
-    x = inputs['start_x'] + i*delta_x
+for i in range(pre_probe_inputs['num_x_points']):
+    x = pre_probe_inputs['start_x'] + i*delta_x
     output_file.write('(X={:5.4f})\n'.format(x))
-    for j in range(inputs['num_a_points']):
+    for j in range(pre_probe_inputs['num_a_points']):
         a = j*delta_a
-        x = inputs['start_x'] + i*delta_x
+        x = pre_probe_inputs['start_x'] + i*delta_x
         output_file.write('G0 X {:5.4f} A {:5.3f}\n'.format(x,a))
-        output_file.write('G31 Z {:5.4f} F {:2.1f}\n'.format(probe_min_z,inputs['probe_feedrate']))
+        output_file.write('G31 Z {:5.4f} F {:2.1f}\n'.format(probe_min_z,pre_probe_inputs['probe_feedrate']))
         output_file.write('G0 Z {:5.4f}\n'.format(safe_z_height))
 
 
