e326db0992409e5271c511533a3835528f51a855
Peter McCloud <peter@mccloudaero.com>
2022-11-19 21:17:33 -0800

    pre_probe_cylinder_plot.py - updated to use pre_plot_cylinder.inputs for R_ref

--- pre_probe_cylinder_plot.py
+++ pre_probe_cylinder_plot.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python
+import os
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
 
 import probe
 
-R_ref = 12.75/2
+script_inputs_file = './pre_probe_cylinder.inputs'
+R_ref = None
+# Read pre_probe_cylinder.inputs
+if os.path.isfile(script_inputs_file):
+    print('Input file exists, loading inputs file\n')
+    exec(open(script_inputs_file).read())
+    R_ref = pre_probe_inputs['outer_diameter']/2.0
+else:
+    print('No pre_probe_cylinder.inputs file found.\nExiting!')
+    sys.exit()
+
 
 # Read in cylinder probe data
 probe_num_X, probe_num_A, probe_X, probe_Z, probe_A = probe.read_cylinder_probe_file('probe_results.txt')
@@ -37,7 +48,8 @@ if probe_dim == 3:
     ax.yaxis.set_ticks(probe_A_values)
     plt.savefig('probe_contour.png')
 
-print('Nominal Radius: {:5.4f}'.format(R_ref))
+if R_ref is not None:
+    print('Nominal Radius: {:5.4f}'.format(R_ref))
 print('Average Radius: {:5.4f}'.format(R_avg))
 
 # Cross Sections
@@ -51,7 +63,8 @@ for i in range(probe_num_X):
     ax.plot(probe_A[i,:],probe_Z[i,:])
     ax.set_xlabel('A (deg)')
     ax.set_ylabel('Z/Radius (in)')
-    plt.axhline(y=R_ref, color='black')
+    if R_ref is not None:
+        plt.axhline(y=R_ref, color='black')
     plt.axhline(y=R_avg, color='gray')
 
 plt.savefig('probe_cross_sections.png')
