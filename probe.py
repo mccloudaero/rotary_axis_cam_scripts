fb0a49929f419fd8316e6592be2f89d05255d3e0
Peter McCloud <peter@mccloudaero.com>
2022-12-30 11:55:19 -0800

    probe.py - added logic to distingish between cylinder and edge probe types

--- probe.py
+++ probe.py
@@ -7,22 +7,40 @@ from scipy import interpolate
 def read_cylinder_probe_file(filename):
 
     X, Y, Z, A = np.loadtxt(filename, unpack=True)
+    print(X,Z,A)
     X_values = np.unique(X)
     num_X = X_values.size
+    Z_values = np.unique(Z)
+    num_Z = Z_values.size
     A_values = np.unique(A)
     num_A = A_values.size
+    print(num_X,num_Z,num_A)
 
-    X = np.reshape(X, (num_X,num_A))
-    A = np.reshape(A, (num_X,num_A))
-    Z = np.reshape(Z, (num_X,num_A))
+    if num_X > 1 and num_Z == 1:
+        # Probe is edge data
+        X = np.reshape(X, (num_Z,num_A))
+        A = np.reshape(A, (num_Z,num_A))
+        Z = np.reshape(Z, (num_Z,num_A))
 
-    # Copy 0 degree data to 360 data
-    X_c1 = np.reshape(X[:,0], (num_X,1))
-    X_final = np.append(X, X_c1, axis=1)
-    Z_c1 = np.reshape(Z[:,0], (num_X,1))
-    Z_final = np.append(Z, Z_c1, axis=1)
-    A_c1 = np.ones(shape=(num_X,1))*360.0
-    A_final = np.append(A, A_c1, axis=1)
+        # Copy 0 degree data to 360 data
+        X_c1 = np.reshape(X[:,0], (num_Z,1))
+        X_final = np.append(X, X_c1, axis=1)
+        Z_c1 = np.reshape(Z[:,0], (num_Z,1))
+        Z_final = np.append(Z, Z_c1, axis=1)
+        A_c1 = np.ones(shape=(num_Z,1))*360.0
+        A_final = np.append(A, A_c1, axis=1)
+    else:
+        X = np.reshape(X, (num_X,num_A))
+        A = np.reshape(A, (num_X,num_A))
+        Z = np.reshape(Z, (num_X,num_A))
+
+        # Copy 0 degree data to 360 data
+        X_c1 = np.reshape(X[:,0], (num_X,1))
+        X_final = np.append(X, X_c1, axis=1)
+        Z_c1 = np.reshape(Z[:,0], (num_X,1))
+        Z_final = np.append(Z, Z_c1, axis=1)
+        A_c1 = np.ones(shape=(num_X,1))*360.0
+        A_final = np.append(A, A_c1, axis=1)
     
     return num_X, num_A, X_final, Z_final, A_final
 
